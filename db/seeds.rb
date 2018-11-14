96f11cc7c8eebe003fd4e100ed20c53af27190e1
Cedric Dlh <cedricdlh@gmail.com>
2018-11-14 21:39:10 +0100

    Set up the controller and routes for yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create([
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'chinese',
+    phone_number:   "30758923759237"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:   "0758923759237"
+  },
+  {
+    name:         'Byron',
+    address:      'Hammersmith W6',
+    category:  'french',
+    phone_number:   "0758923759237"
+  },
+  {
+    name:         'Charlotte 1',
+    address:      'Check Point Charlie',
+    category:  'french',
+    phone_number:   "0758923759237"
+  },
+  {
+    name:         'Chupenga',
+    address:      'Check Point Charlie 2',
+    category:  'belgian',
+    phone_number:   "0758923759237"
+  }
+  ])
+
